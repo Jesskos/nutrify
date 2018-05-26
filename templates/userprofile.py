ba96233ebe21ff27bb09e0adccfc299f47084c21
vagrant <vagrant@vagrant.vm>
2018-05-26 02:36:15 +0000

    changes made

---
+++ templates/userprofile.py
@@ -0,0 +1,12 @@
+ {% extends 'base2.html' %}
+
+{% block title %} View Saved Recipes  {% endblock %}
+
+{% block content %}
+
+
+<h3> {{ fname }} </h3>
+<h3> {{ lname }} </h3>
+<h3> {{ email }} </h3>
+
+{% endblock %}
